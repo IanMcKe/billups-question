99512650c76008adca1fb78dc9a4ca21b710059d
Ian McKenney <ian.jafool@gmail.com>
2016-03-08 15:58:22 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var rpslsGame = function(player1, player2) {
+  //Draw
+  if(player1 == player2) {
+    return "Draw!";
+  }
+  //Rock Wins
+
+  //Paper Wins
+
+  //Scissors Wins
+
+  //Lizard Wins
+
+  //Spock Wins
+}
