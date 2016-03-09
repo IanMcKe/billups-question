d6662c740406eb6e8ea7e42ce8665a07fb1c9e9c
Ian McKenney <ian.jafool@gmail.com>
2016-03-08 17:20:31 -0800

    refactor to use generalized/more optimized solution rather than brute force the problem

--- scripts.js
+++ scripts.js
@@ -1,43 +1,26 @@
+var choices = ["rock", "scissors", "lizard", "paper", "spock"];
+
+function mod(a, b) {
+  var c = a % b;
+  if(c < 0) {
+    return c + b;
+  } else {
+    return c;
+  }
+}
+
 var rpslsGame = function(player1, player2) {
   //Draw
   if(player1 == player2) {
     return "Draw!";
   }
 
-  //Rock Wins
-  if(player1 == "rock" && player2 == "scissors") {
+  var player1Index = choices.indexOf(player1);
+  var player2Index = choices.indexOf(player2);
+
+  if(mod((player1Index - player2Index), choices.length) > choices.length/2) {
     return "Player 1 Wins!";
-  } else if(player1 == "rock" && player2 == "lizard") {
-    return "Player 1 Wins!";
-  } else if(player1 == "scissors" && player2 == "rock") {
-    return "Player 2 Wins!";
-  } else if (player1 == "lizard" && player2 == "rock") {
+  } else {
     return "Player 2 Wins!";
   }
-
-  //Paper Wins
-  else if(player1 == "paper" && player2 == "rock") {
-    return "Player 1 Wins!";
-  } else if(player1 == "paper" && player2 == "spock") {
-    return "Player 1 Wins!";
-  } else if(player1 == "rock" && player2 == "paper") {
-    return "Player 2 Wins!";
-  } else if(player1 == "spock" && player2 == "paper") {
-    return "Player 2 Wins!";
-  }
-
-  //Scissors Wins
-  else if(player1 == "scissors" && player2 == "paper") {
-    return "Player 1 Wins!";
-  } else if(player1 == "scissors" && player2 == "lizard") {
-    return "Player 1 Wins!";
-  } else if(player1 == "paper" && player2 == "scissors") {
-    return "Player 2 Wins!";
-  } else if(player1 == "lizard" && player2 == "scissors") {
-    return "Player 2 Wins!";
-  }
-
-  //Lizard Wins
-
-  //Spock Wins
 }
