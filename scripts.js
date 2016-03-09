ef869a64d94e2bb6c5c0aa4b1311f71e694931ec
Ian McKenney <ian.jafool@gmail.com>
2016-03-08 16:08:52 -0800

    rock wins specs added and passed

--- scripts.js
+++ scripts.js
@@ -4,7 +4,15 @@ var rpslsGame = function(player1, player2) {
     return "Draw!";
   }
   //Rock Wins
-
+  if(player1 == "rock" && player2 == "scissors") {
+    return "Player 1 Wins!";
+  } else if (player1 == "rock" && player2 == "lizard") {
+    return "Player 1 Wins!";
+  } else if(player1 == "scissors" && player2 == "rock") {
+    return "Player 2 Wins!";
+  } else if (player1 == "lizard" && player2 == "rock") {
+    return "Player 2 Wins!";
+  }
   //Paper Wins
 
   //Scissors Wins
--- tests/specs.js
+++ tests/specs.js
@@ -1,1 +1,25 @@
+describe('rpslsGame', function() {
+  //Draw case
+  it('returns "Draw!" if both players enter the same choice', function() {
+    expect(rpslsGame("rock", "rock")).to.equal("Draw!");
+  });
 
+  //Rock Wins cases
+  it('Player 1 wins if he or she enters "rock" and Player 2 enters "scissors"', function() {
+    expect(rpslsGame("rock", "scissors")).to.equal("Player 1 Wins!");
+  });
+
+  it('Player 1 wins if he or she enters "rock" and Player 2 enters "lizard"', function() {
+    expect(rpslsGame("rock", "lizard")).to.equal("Player 1 Wins!");
+  });
+
+  it('Player 2 wins if he or she enters "rock" and Player 1 enters "scissors"', function() {
+    expect(rpslsGame("scissors", "rock")).to.equal("Player 2 Wins!");
+  });
+
+  it('Player 2 wins if he or she enters "rock" and Player 1 enters "lizard"', function() {
+    expect(rpslsGame("lizard", "rock")).to.equal("Player 2 Wins!");
+  });
+
+  //Paper Wins cases
+});
