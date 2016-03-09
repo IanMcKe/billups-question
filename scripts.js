580cb49c5a9452aa3a6e47032fe44a8b7044a68f
Ian McKenney <ian.jafool@gmail.com>
2016-03-08 18:23:21 -0800

    done with initial version pending styling

--- scripts.js
+++ scripts.js
@@ -9,6 +9,11 @@ function mod(a, b) {
   }
 }
 
+function computerChoice() {
+  var rand = Math.floor(Math.random() * 5);
+  return choices[rand];
+}
+
 var rpslsGame = function(player1, player2) {
   //Draw
   if(player1 == player2) {
@@ -24,3 +29,24 @@ var rpslsGame = function(player1, player2) {
     return "Player 2 Wins!";
   }
 }
+
+$(document).ready(function() {
+  $("form button[type=submit]").click(function() {
+    $("button[type=submit]", $(this).parents("form")).removeAttr("clicked");
+    $(this).attr("clicked", "true");
+  });
+
+  $("form").submit(function(event) {
+    var player1 = $("button[type=submit][clicked=true]").val();
+    var computer = computerChoice();
+    var result = rpslsGame(player1, computer);
+
+    if(result == "Player 2 Wins!") {
+      $("div#result").text("Computer Wins! You chose: " + player1 + " the computer chose: " + computer);
+    } else {
+      $("div#result").text("You Win!" + " You chose: " + player1 + " the computer chose: " + computer);
+    }
+
+    event.preventDefault();
+  });
+});
