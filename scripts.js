2cea573e45c71232357f5b0cea5f9990aa5e7f21
Ian McKenney <ian.jafool@gmail.com>
2016-03-08 16:37:52 -0800

    scissors wins specs added and passed

--- scripts.js
+++ scripts.js
@@ -3,27 +3,39 @@ var rpslsGame = function(player1, player2) {
   if(player1 == player2) {
     return "Draw!";
   }
+
   //Rock Wins
   if(player1 == "rock" && player2 == "scissors") {
     return "Player 1 Wins!";
-  } else if (player1 == "rock" && player2 == "lizard") {
+  } else if(player1 == "rock" && player2 == "lizard") {
     return "Player 1 Wins!";
   } else if(player1 == "scissors" && player2 == "rock") {
     return "Player 2 Wins!";
   } else if (player1 == "lizard" && player2 == "rock") {
     return "Player 2 Wins!";
   }
+
   //Paper Wins
-  else if (player1 == "paper" && player2 == "rock") {
+  else if(player1 == "paper" && player2 == "rock") {
     return "Player 1 Wins!";
-  } else if (player1 == "paper" && player2 == "spock") {
+  } else if(player1 == "paper" && player2 == "spock") {
     return "Player 1 Wins!";
-  } else if (player1 == "rock" && player2 == "paper") {
+  } else if(player1 == "rock" && player2 == "paper") {
     return "Player 2 Wins!";
-  } else if (player1 == "spock" && player2 == "paper") {
+  } else if(player1 == "spock" && player2 == "paper") {
     return "Player 2 Wins!";
   }
+
   //Scissors Wins
+  else if(player1 == "scissors" && player2 == "paper") {
+    return "Player 1 Wins!";
+  } else if(player1 == "scissors" && player2 == "lizard") {
+    return "Player 1 Wins!";
+  } else if(player1 == "paper" && player2 == "scissors") {
+    return "Player 2 Wins!";
+  } else if(player1 == "lizard" && player2 == "scissors") {
+    return "Player 2 Wins!";
+  }
 
   //Lizard Wins
 
